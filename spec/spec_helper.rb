636fe2ca7b950a5ac7d6359e9e7b2dc4237bf99d
Tyler Westlie <tyler.westlie@gmail.com>
2018-05-01 15:45:04 -0600

    rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
 ENV["RACK_ENV"] ||= "test"
 
+require 'bundler'
+Bundler.require(:default, :test)
 require File.expand_path("../../config/environment", __FILE__)
-require 'rspec'
+
 require 'capybara/dsl'
+require 'rspec'
 
 Capybara.app = LittleShopApp
 
