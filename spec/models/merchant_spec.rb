c3f726fa8c4a4d93d51d1eef044d49d50130a89b
apoc64 <sschwedt@gmail.com>
2018-05-01 17:52:46 -0600

    tests it can successfully add valid merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,8 +3,12 @@ RSpec.describe Merchant do
     describe 'required fields' do
       it 'should be invalid if missing name' do
         merchant = Merchant.create(merchant_id: 1)
-
         expect(merchant).to_not be_valid
+      end
+
+      it 'should add a merchant with a name' do
+        merchant = Merchant.create(name: 'Cole', merchant_id: 1)
+        expect(merchant).to be_valid
       end
     end
   end
