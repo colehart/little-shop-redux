b1fed0d6c645dae87e23660ea6bc6e13deac3666
Tyler Westlie <tyler.westlie@gmail.com>
2018-05-01 15:43:13 -0600

    added bundler, rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,14 @@ require 'capybara/dsl'
 
 Capybara.app = LittleShopApp
 
+DatabaseCleaner.strategy = :truncation
+
 RSpec.configure do |c|
   c.include Capybara::DSL
+  c.before(:all) do
+    DatabaseCleaner.clean
+  end
+  c.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
