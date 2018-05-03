8d3e2767e3c4ef0f57b5ac927ad24c9a51f71c80
apoc64 <sschwedt@gmail.com>
2018-05-02 20:16:11 -0600

    debugging determined test db works, not dev/prod

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -1,9 +1,12 @@
 class LittleShopApp < Sinatra::Base
 
   get '/merchants' do
+    # require 'pry'; binding.pry
+    Merchant.create(name: 'Cole', merchant_id: 1)
+
     @merchants = Merchant.all
     erb :"merchants/index"
   end
-  
+
 
 end
