8ddfa2181e2fa230f8d719374cb3b2081f17d04a
Cole Hart <cole@colehart.com>
2018-05-06 12:58:51 -0600

    Unsure of how to write a test to see disabled status of css for merchant dropdown on the edit invoice page. All other tests passing for iter 5/6. Still need to show tables of items on each invoice.

--- spec/features/invoices/user_can_edit_existing_invoice_spec.rb
+++ spec/features/invoices/user_can_edit_existing_invoice_spec.rb
@@ -9,16 +9,14 @@ RSpec.describe 'A user can edit an invoice' do
     expect(page).to have_content(invoice.status.capitalize)
   end
 
-# Unsure of how to write a Capy test for disabled merchants dropdown
-  # it 'disables the merchant drop-down while highlighting the right merchant' do
-  #   merchant = Merchant.create(name: 'Cole')
-  #   invoice = Invoice.create(customer_id: 1, merchant_id: 1, status: 'shipped')
-  #
-  #   visit "/invoices/#{invoice.id}/edit"
-  #
-  #   save_and_open_page
-  #   expect(page).to have_css('.merchant-dropdown = disabled')
-  # end
+  it 'disables the merchant drop-down while highlighting the right merchant' do
+    Merchant.create(name: 'Cole')
+    invoice = Invoice.create(customer_id: 1, merchant_id: 1, status: 'shipped')
+
+    visit "/invoices/#{invoice.id}/edit"
+
+    expect(page).to have_css('.merchant-dropdown  disabled')
+  end
 
   it 'shows edit an invoice status' do
     Merchant.create(name: 'Cole')
