73de560e827bfb2ccda522b2c166b499cfcf3eb7
apoc64 <sschwedt@gmail.com>
2018-05-08 16:25:11 -0600

    tests calculate total invoices by status

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -28,16 +28,16 @@ RSpec.describe Invoice do
   end
 
   describe 'class methods' do
-    it 'can calculate total price' do
-      invoice = Invoice.create(customer_id: 1, merchant_id: 1, status: 'shipped')
-      Item.create(name:'bork', description:'totally borked it', unit_price:666, merchant_id:1, image:'borkface.jpeg')
-      Item.create(name:'dork', description:'totally dorked it', unit_price:667, merchant_id:1, image:'dorkface.jpeg')
-      invoice_item = InvoiceItem.create(item_id: 1, invoice_id: 1, quantity: 3, unit_price: 1500)
-      invoice_item2 = InvoiceItem.create(item_id: 2, invoice_id: 1, quantity: 2, unit_price: 1000)
+    it 'can calculate total invoices by status' do
+      Invoice.create(customer_id: 1, merchant_id: 1, status: 'shipped')
+      Invoice.create(customer_id: 2, merchant_id: 4, status: 'pending')
+      Invoice.create(customer_id: 4, merchant_id: 5, status: 'pending')
 
-      total = (invoice_item.quantity * invoice_item.unit_price) + (invoice_item2.quantity * invoice_item2.unit_price)
+      percent1 = Invoice.total_invoices_by_status('pending')
+      percent2 = Invoice.total_invoices_by_status('shipped')
 
-      expect(invoice.total_price).to eq(total)
+      expect(percent1).to eq(66)
+      expect(percent2).to eq(33)
     end
   end
 end
