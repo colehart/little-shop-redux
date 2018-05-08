6ad3a2a5c335001dd60a108c92ae89841f99340f
apoc64 <sschwedt@gmail.com>
2018-05-08 11:50:53 -0600

    shows items for merchant

--- spec/features/merchants/user_can_see_single_merchant_spec.rb
+++ spec/features/merchants/user_can_see_single_merchant_spec.rb
@@ -22,4 +22,18 @@ RSpec.describe 'A user can see one merchant' do
     expect(current_path).to eq('/merchants')
     expect(page).to_not have_content(merchant)
   end
+
+  it 'shows list of merchant items' do
+    merchant = Merchant.create(name: 'Cole')
+    item_name = 'bork'
+    merchant.items.create(name: item_name, description: 'totally borked it', unit_price: 666, image: 'borkface.jpeg')
+    unit_price = 1000
+    merchant.items.create(name: 'dork', description: 'totally dorked it', unit_price: unit_price, image: 'dorkface.jpeg')
+
+    visit "/merchants/#{merchant.id}"
+    save_and_open_page
+    # binding.pry
+    expect(page).to have_content(item_name)
+    expect(page).to have_content(unit_price)
+  end
 end
